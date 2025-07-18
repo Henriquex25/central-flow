47a652001bf526d55a77b9ceabf2ac9ce808df52
Henrique Miranda <sergioh003@gmail.com>
2025-07-18 19:06:43 -0300

    Instalado o tailwind v4

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 <template>
     <div class="p-8">
         <h1 class="text-2xl font-bold text-blue-600">Olá, Electron + Vue + Tailwind! 🚀</h1>
+        <div class="text-purple-500 mt-4">Teste</div>
     </div>
 </template>
 
